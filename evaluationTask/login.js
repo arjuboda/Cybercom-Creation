ae8261c640edd9f1c6a775a45ccc46f2657220fb
arjuboda <arjuben.boda111069@marwadiuniversity.ac.in>
2024-02-02 17:05:08 +0530

    store data in local storage

--- evaluationTask/login.js
+++ evaluationTask/login.js
@@ -3,10 +3,11 @@ function openRegistrationPage() {
     // alert('hrllo');
     window.location.href = 'registration.html'
 }
+let currentUserName;
+let currentUserBirthDate;
+
+// $('#loginForm').submit(function () {
 function redirectToDashboard() {
-    // alert();
-    // let Name = localStorage.getItem('userEmail');
-    // let password = localStorage.getitem('userPassword');
     let emlEmail = document.getElementById('emlEmail').value;
     let pswPassword = document.getElementById('pswPassword').value;
     let adminEmail = 'admin@abc.com';
@@ -15,11 +16,40 @@ function redirectToDashboard() {
         alert('please fill the fields')
     }
     else if (emlEmail === adminEmail && pswPassword == adminPassword) {
-
         window.location.href = 'adminDashboard.html';
-
     }
     else {
-        window.location.href = 'user.html';
+        let nameFind = false;
+        let passwordFails = false;
+        let emlEmail = document.getElementById('emlEmail').value;
+        let pswPassword = document.getElementById('pswPassword').value;
+        let UserData = JSON.parse(localStorage.getItem('userData')) || [];
+        // console.log("Email:", emlEmail);
+        // console.log("Password:", pswPassword);
+        // console.log("UserData:", UserData);
+        UserData.forEach(element => {
+            if (element.userEmail == emlEmail && element.userPassword == pswPassword) {
+                currentUserName = element.userName;
+                currentUserBirthDate = element.userBirthDate;
+                nameFind = true;
+            }
+            else if (element.userEmail == emlEmail) {
+                passwordFails = true;
+            }
+        });
+
+        if (nameFind) {
+            JSON.stringify(sessionStorage.setItem('currentUserName', currentUserName));
+            JSON.stringify(sessionStorage.setItem('currentUserBirthDate', currentUserBirthDate));
+            window.location.href = 'userDashboard.html';
+        }
+        else if (passwordFails) {
+            alert('check your email id and password');
+            event.preventDefault();
+        }
+        else {
+            alert('First Register Your self');
+            window.location.href = 'registration.html';
+        }
     }
 }

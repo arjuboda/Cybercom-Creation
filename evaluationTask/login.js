e5807ac23ac25d8747bebdb8fe9b2747b056495d
arjuboda <arjuben.boda111069@marwadiuniversity.ac.in>
2024-02-05 11:13:17 +0530

    add user login time to userData

--- evaluationTask/login.js
+++ evaluationTask/login.js
@@ -1,12 +1,17 @@
 
 function openRegistrationPage() {
-    // alert('hrllo');
     window.location.href = 'registration.html'
 }
+
+// Function to get current  time for a user login
+function storeLoginTime(userid) {
+    let date = new Date();
+    let time = date.toString().split(' ')[4];
+    return time; //return current time
+}
+
 let currentUserName;
 let currentUserBirthDate;
-
-// $('#loginForm').submit(function () {
 function redirectToDashboard() {
     let emlEmail = document.getElementById('emlEmail').value;
     let pswPassword = document.getElementById('pswPassword').value;
@@ -24,30 +29,30 @@ function redirectToDashboard() {
         let emlEmail = document.getElementById('emlEmail').value;
         let pswPassword = document.getElementById('pswPassword').value;
         let UserData = JSON.parse(localStorage.getItem('userData')) || [];
-        // console.log("Email:", emlEmail);
-        // console.log("Password:", pswPassword);
-        // console.log("UserData:", UserData);
+
         UserData.forEach(element => {
             if (element.userEmail == emlEmail && element.userPassword == pswPassword) {
                 currentUserName = element.userName;
                 currentUserBirthDate = element.userBirthDate;
+                element.userLoginTime = storeLoginTime(element.userId); //set login time
                 nameFind = true;
             }
-            else if (element.userEmail == emlEmail) {
+            else if (element.userEmail == emlEmail) { //email matched but not password
                 passwordFails = true;
             }
         });
 
-        if (nameFind) {
+        if (nameFind) { //email and password both matched
             JSON.stringify(sessionStorage.setItem('currentUserName', currentUserName));
             JSON.stringify(sessionStorage.setItem('currentUserBirthDate', currentUserBirthDate));
+            localStorage.setItem('userData', JSON.stringify(UserData));
             window.location.href = 'userDashboard.html';
         }
         else if (passwordFails) {
-            alert('check your email id and password');
+            alert('Email Id and Password not matched');
             event.preventDefault();
         }
-        else {
+        else { //if email not matched then redirect to register page
             alert('First Register Your self');
             window.location.href = 'registration.html';
         }

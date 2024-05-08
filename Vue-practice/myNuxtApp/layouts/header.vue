9f90f274edd3b0097f34c7b65f7a7b8203d07400
arjuboda <arjuben.boda111069@marwadiuniversity.ac.in>
2024-05-08 14:18:55 +0530

    practice basics of Nuxt2

---
+++ Vue-practice/myNuxtApp/layouts/header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div  class="bg-gray-800 py-4">
+        <header>
+            <NuxtLink to="/">MyAPP</NuxtLink>
+            <nav>
+                <NuxtLink to="/">Home</NuxtLink>
+                <NuxtLink to="/login">Lgin</NuxtLink>
+                <NuxtLink to="/signup">Signup</NuxtLink>
+                <NuxtLink to="/cart">Cart</NuxtLink>
+            </nav>
+        </header>
+        <Nuxt/>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Vue-practice/myNuxtApp/pages/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>hey</h1>
+      <h1>{{ slug }}</h1>
+  </div>
+  </template>
+  
+  <script>
+    export default {
+      async asyncData({ params }) {
+        const slug = params.slug // When calling /abc the slug will be "abc"
+        return { slug }
+      },
+      layout:'header',
+    }
+  </script>
